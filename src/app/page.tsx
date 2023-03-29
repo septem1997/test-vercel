5577c123f4ddc3cdb2397fdacc2659b8dd6e046b
septem1997 <750627595@qq.com>
2023-03-29 13:26:46 +0800

    test vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <Link href={"/test"}>go to test</Link>
+        <Link href={"/test"}>I'm cool girl</Link>
       </div>
     </main>
   )
