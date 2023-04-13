fdeb323f51ebf8e0f68b641e77b401e6222ea271
septem1997 <750627595@qq.com>
2023-04-13 15:04:06 +0800

    test vercel

--- src/pages/api/og/test.tsx
+++ src/pages/api/og/test.tsx
@@ -11,6 +11,7 @@ export default function handler(request:Request){
             <div tw={"flex relative w-full h-full bg-transparent"}>
                 <div tw={"absolute left-[32px] bottom-[32px] mb-[32px] flex flex-col text-white max-w-[300px]"}>
                     Not Found
+                    <img src={"https://avatars.githubusercontent.com/u/13849245?v=4"} />
                 </div>
             </div>
         ),
