f61654c32d30b390d574718ce42966f3699db53a
septem1997 <750627595@qq.com>
2023-04-13 14:44:43 +0800

    test vercel

--- src/pages/api/og/test.tsx
+++ src/pages/api/og/test.tsx
@@ -1,5 +1,10 @@
 import {ImageResponse} from "@vercel/og";
 
+
+export const config = {
+    runtime: "edge",
+};
+
 export default function handler(request:Request){
     return new ImageResponse(
         (
